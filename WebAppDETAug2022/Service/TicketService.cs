1fccafd2c81c5b49151932fda7d0aeecb20f40cf
Monika <monika.gmony@gmail.com>
2022-09-07 17:56:47 +0530

    Add project files.

---
+++ WebAppDETAug2022/Service/TicketService.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+using WebAppDETAug2022.Models;
+
+namespace WebAppDETAug2022.Service
+{
+    public class TicketService
+    {
+        static List<Ticket> Tickets { get; }
+
+        static TicketService()
+        {
+            Tickets = new List<Ticket> {
+            new Ticket { Category = "platinumplus", Price = 5000, MaxLimit = 200000 },
+            new Ticket { Category = "platinum", Price = 4000, MaxLimit = 300000 },
+            new Ticket { Category = "Gold", Price = 3000, MaxLimit = 500000 },
+            new Ticket { Category = "silver", Price = 2000, MaxLimit = 500000 },
+            new Ticket { Category = "General", Price = 800, MaxLimit = 100000 },
+            };
+        }
+
+        public static List<Ticket> GetAll() => Tickets;
+       
+    }
+}
