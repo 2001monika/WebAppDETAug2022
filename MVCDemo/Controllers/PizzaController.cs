66e34026e4dff4e064c865ac78de868e781392ad
Monika <monika.gmony@gmail.com>
2022-09-08 18:27:38 +0530

    Asp.Net

---
+++ MVCDemo/Controllers/PizzaController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+using MVCDemo.Service;
+
+namespace MVCDemo.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Pizza> pizzas = PizzaService.GetAll();
+
+            return View(pizzas);
+        }
+        public IActionResult Details(int id)
+        {
+            Pizza p = PizzaService.Get(id);
+            return View(p);
+        }
+
+        public IActionResult List()
+        {
+            List<Pizza> pizzas = PizzaService.GetAll();
+
+            return View(pizzas);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(Pizza p)
+        {
+            PizzaService.Add(p);
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            Pizza p=PizzaService.Get(Id);
+            if (p != null)
+                return View(p);
+            else
+                return RedirectToAction("List");
+        }
+        [HttpPost]
+        public IActionResult Delete(Pizza p)
+        {
+            PizzaService.Delete(p.Id);
+            return RedirectToAction("List");
+        }
+
+    }
+
+}
